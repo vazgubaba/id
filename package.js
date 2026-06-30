7315d59cd9447f764f9de330c50c64c76ea16700
vazgubaba <hirakildis@gmail.com>
2026-06-30 10:13:01 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,26 @@
+{
+  "name": "fivem-id-bot",
+  "version": "1.0.0",
+  "description": "Mini Discord bot - .id, .tag, .sesgir komutları",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "keywords": [
+    "discord",
+    "bot",
+    "discordjs",
+    "fivem"
+  ],
+  "author": "Woopiè",
+  "license": "ISC",
+  "dependencies": {
+    "@discordjs/voice": "^0.17.0",
+    "discord.js": "^14.15.3",
+    "express": "^4.19.2",
+    "node-fetch": "^2.7.0"
+  },
+  "engines": {
+    "node": "20.x"
+  }
+}
